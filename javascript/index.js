17f195c5ab4048310137094d2f56a83b7e10f617
Silésio Leudério Cipriano <silesioleuderiocipriano@hotmail.com>
2023-02-08 16:49:36 +0200

    html & style: scroll button

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,8 @@
 let nav = document.getElementById('nav');
 let centerNav = document.getElementById('center_nav');
 let content = document.getElementById('content');
+let goDown = document.getElementById('goDown');
+let goUp = document.getElementById('goUp');
 let showSidebar = false;
 
 function toggleSidebar() {
@@ -25,3 +27,21 @@ window.addEventListener('resize', function (event) {
     toggleSidebar();
   }
 });
+
+var lastScrollTop = 0;
+
+window.addEventListener(
+  'scroll',
+  function () {
+    var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+    if (st > lastScrollTop) {
+      goUp.style.display = 'block';
+      goDown.style.display = 'none';
+    } else if (st < lastScrollTop) {
+      goUp.style.display = 'none';
+      goDown.style.display = 'block';
+    } // else was horizontal scroll
+    lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+  },
+  false
+);
